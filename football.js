c4c960c73b334705cd9b931d2b711d78a5ea020d
alkimvyt <vytautas.alkimavicius@affecto.com>
2015-10-28 18:23:03 +0200

    team list extraction

--- football.js
+++ football.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function() {
 	var element = $(".so_not_played")[0];
 	if (element) {
@@ -5,4 +7,32 @@ $(document).ready(function() {
 		      scrollTop: $(element).offset().top
 		}, 10);
 	}
+
+	var teams = Utils.extractTeams();
+    console.log(teams);
+
 });
+
+var Utils = {
+	extractTeams : {
+		var result = [];
+		var teamsPushed = [];
+		$.each($(".match-day .team-h a"), function(index, element) {
+			var teamName = $(element).html();
+			if ($.inArray(teamName, teamsPushed) == -1) {
+				teamsPushed.push(teamName);
+				var href = $(element).attr("href");
+				var img = $(element).parent().parent().find(".team-embl img").attr("src");
+				result.push(new Team(teamName, href, img));
+			}
+		});
+		return result;
+	}
+}
+
+
+var Team = function(name, url, img) {
+	this.name = name;
+	this.url = url;
+	this.img = img;
+}
