79a43e04b551c89d9367f6db01c5ef03cd00c457
vytalk <vytautas.alkimavicius@gmail.com>
2015-11-08 14:06:08 +0200

    refactoring js

--- football.js
+++ football.js
@@ -1,58 +1,89 @@
 'use strict';
 
 $(document).ready(function() {
-	var element = $(".so_not_played")[0];
-	if (element) {
-		$('html, body').animate({
-		      scrollTop: $(element).offset().top
-		}, 10);
-	}
+	
+	var fth = new FootballTvHelper();
 
-	var teams = Utils.extractTeams();
-    Utils.generate(teams);
+	fth.scrollToComingGames();
+    fth.generateClickableTeamsList();
     
 });
 
-var Utils = {
-	extractTeams : function() {
+var FootballTvHelper = function() {
+
+	this.scrollToComingGames = function() {
+		var element = $(".so_not_played")[0];
+		if (element) {
+			$('html, body').animate({
+		      scrollTop: $(element).offset().top
+			}, 10);	
+		}
+	};
+
+	this.generateClickableTeamsList = function() {
+		var teams = extractTeams();
+		generateList(teams);
+	};
+
+	this.showTeamMatches = function(teamName) {
+		$.each($(".match-day tr"), function(index, element) {
+			if (doHaveElementHaveTeam(teamName, element)) {
+				$(element).show();
+			} else {
+				$(element).hide();
+			}
+		});
+	};
+
+	var self = this;
+
+	var Team = function(name, img) {
+		this.name = name;
+		this.img = img;
+	};
+
+	var extractTeams = function() {
 		var result = [];
 		var teamsPushed = [];
-		$.each($(".match-day .team-h a"), function(index, element) {
-			var teamName = $(element).html();
+		$.each($(".match-day .team-embl img"), function(index, element) {
+			var teamName = $(element).attr("alt");
+			var img = $(element).attr("src");
+
 			if ($.inArray(teamName, teamsPushed) == -1) {
 				teamsPushed.push(teamName);
-				var href = $(element).attr("href");
-				var img = $(element).parent().parent().find(".team-embl img").attr("src");
-				result.push(new Team(teamName, href, img));
+				result.push(new Team(teamName, img));
 			}
 		});
 		return result;
-	},
+	};
 
-	generate : function(teams) {
+	var generateList = function(teams) {
 		$.get(chrome.extension.getURL("teambox.html"), function(data) {
+
 			var template = Handlebars.compile(data);
 			var html = template({list : teams});
 			$("body").append(html);
+
 			$(".box-team").click(function() {
 				var teamName = $(this).find("img").attr("alt");
-				var elementsToShow = [];
-				$.each($(".match-day tr"), function(index, element) {
-					var currentTeams = $(element).find(".team-embl img");
-					if (currentTeams && currentTeams.length > 0 && ($(currentTeams[0]).attr("alt") === teamName || $(currentTeams[1]).attr("alt") === teamName)) {
-						$(element).show();
-					} else {
-						$(element).hide();	
-					}
-				});
+				self.showTeamMatches(teamName);
 			});
 		});
+	};
+
+	var doHaveElementHaveTeam = function(teamName, element) {
+		var currentTeams = $(element).find(".team-embl img");
+		if (currentTeams.length > 0) {
+			var has = false;
+			$.each(currentTeams, function(index, team) {
+				if ($(team).attr("alt") === teamName) {
+					has = true;
+					return false;
+				} 
+			});
+			return has;
+		} else {
+			return false;
+		}
 	}
-}
-
-
-var Team = function(name, url, img) {
-	this.name = name;
-	this.url = url;
-	this.img = img;
 }
